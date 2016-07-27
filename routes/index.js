c4a25a420a3c35ba11db96aaa2c128bb94c0f88b
half-shell <michel.maubert.pro@gmail.com>
2016-07-27 14:59:12 +0200

    Add management for all routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,13 @@
 module.exports = function(api){
     var users = require('./users')(api);
     api.use('/api/users', users);
+
+    var posts = require('./posts')(api);
+    api.use('/api/posts', posts);
+
+    var pets = require('./pets')(api);
+    api.use('/api/pets', pets);
+
+    var comments = require('./comments')(api);
+    api.use('/api/comments')(api);
 }
