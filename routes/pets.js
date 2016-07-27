61b6303a825f9a1d5a043fe7cca6cf8ea0245138
Nicolas <nicolas.sirac@orange.fr>
2016-07-27 14:53:52 +0200

    pets

---
+++ routes/pets.js
@@ -0,0 +1,54 @@
+var router = require('express').Router();
+var models = require ('../models');
+var Pets = models.Pet;
+module.exports = function(api){
+	router.get('/',function(req,res,next){
+		Pet.findAll().then(function(pets){
+			res.send(pets);
+		})
+		.catch(function(err){
+			res.send(err);
+			
+		});
+	}).get('/:id', function(req, res, next){
+        Pet.findById(req.params.id)
+            .then(function(pet){
+                res.send(pet);
+            })
+        .catch(function(err){
+            res.send(err);
+        });
+    })
+	  .post('/', function(req, res, next){
+        Pet.create({
+            name: req.body.name,
+            type: req.body.type
+			born: req.body.born
+        })
+        .then(function(pet){
+            res.send({success: "Pet added successfuly", pet})
+        })
+        .catch(function(err){
+            res.send(err)
+        })
+    })
+
+    .delete('/:id', function(req, res, next){
+        Pet.findById(req.params.id)
+            .then(function(pet){
+                pet.destroy()
+                    .then(function(rows){
+                        res.send({success:"Pet deleted successfuly"});
+                    })
+                .catch(function(err){
+                    res.send(err);
+                })
+            })
+        .catch(function(err){
+            res.send(err);
+        })
+    });
+
+
+    return router;
+}
